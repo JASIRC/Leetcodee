d42bebda5d15d034beedde2be17b5a669cc62eb8
JASIR C <134064928+JASIRC@users.noreply.github.com>
2024-05-12 11:34:51 +0530

    Time: 68 ms (9.93%), Space: 50.2 MB (68.88%) - LeetHub

---
+++ 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} candies
+ * @param {number} extraCandies
+ * @return {boolean[]}
+ */
+var kidsWithCandies = function(candies, extraCandies) {
+    let max = candies.reduce((a,b) => Math.max(a,b), -Infinity);
+    let res = [];
+    for(let i=0; i<candies.length; i++) {
+        if(extraCandies+candies[i] >= max) res[i] = true;
+        else res[i] = false;
+    }
+    return res;
+};
